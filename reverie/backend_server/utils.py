916cb7a195ed4d38919abe63a455b240198df6fc
ndndkxjsk <140129361+ndndkxjsk@users.noreply.github.com>
2023-08-20 22:51:11 +0800

    Update utils.py

--- reverie/backend_server/utils.py
+++ reverie/backend_server/utils.py
@@ -1,7 +1,14 @@
-# Copy and paste your OpenAI API Key
-openai_api_key = "<Your OpenAI API>"
+import os
+
+# Fetch the OpenAI API Key from the environment variable
+openai_api_key = os.environ.get('OPENAI_API_KEY', 'Default_Value_If_Not_Found')
+# If you want the script to fail if the key is not set, you can use:
+# openai_api_key = os.environ['OPENAI_API_KEY']
+
 # Put your name
-key_owner = "<Name>"
+key_owner = os.environ.get('KEY_OWNER', 'Default_Name_If_Not_Found')
+# If you want the script to fail if the name is not set, you can use:
+# key_owner = os.environ['KEY_OWNER']
 
 maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
